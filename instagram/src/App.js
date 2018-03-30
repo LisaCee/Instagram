dcbc0f0500eeb0d1f23d67f29d8cd3eb9881e2d7
Stephanie <sbowl001@fiu.edu>
2018-03-30 10:26:30 +0800

    commentsection

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 import './App.css';
-// import dummyData from './dummy-data';
-// import PostContainer from './components/CommentSection/CommentSection';
+import dummyData from './dummy-data';
+import PostContainer from './components/CommentSection/CommentSection';
 // import SearchBar from './components/SearchBar/SearchBar';
-import CommentSection from './components/CommentSection/CommentSection';
+// import CommentSection from './components/CommentSection/CommentSection';
 
 const App = () => { 
     return <div className="App">
-        {/* <SearchBar />
+        {/* <SearchBar /> */}
         {dummyData.map((post, i) => (
           <PostContainer postData={post} key={i} />
-        ))} */}
-        <CommentSection />
+        ))}
+        {/* <CommentSection /> */}
       </div>;
       
   }
