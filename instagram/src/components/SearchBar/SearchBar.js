49a859428443b071eb7fc0baa34c2d501b4e503e
Lisa Cee <three_nineteen@yahoo.com>
2018-04-05 19:55:37 -0700

    more styling for searchbar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -16,11 +16,15 @@ class SearchBar extends Component {
     render() {
         return (
                 <Row className='search'>
-                    <i class="fab fa-instagram fa-lg"></i> |
+                    <i class="fab fa-instagram fa-lg"></i> 
+                    <div className='pipe'>|</div>
                     <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/640px-Instagram_logo.svg.png' alt='Instagram name in cursive' className='name'/>
-                    <div className='search-w-icon'>
-                        <input type='text' placeholder='Search'></input>
+
+                    <div className='inputWithIcon'>
+                        <input type='text' placeholder='Search' />
+                        <span><i class="fab fa-instagram fa-lg"></i> </span>    
                     </div>
+
                     <i class="far fa-compass"></i>
                     <i class="far fa-heart"></i>
                     <i class="far fa-user"></i>
